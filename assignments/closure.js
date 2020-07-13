727b91f29b158938b476f971649f2cc496c4d880
Karan Chouhan <karan.chouhan@mountblue.tech>
2020-07-13 11:45:58 +0000

    Update closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -20,23 +20,28 @@ console.log(counterFactory.decrement());
 //#2
 function limitFunctionCallCount(n,cb) {
    function invoke () {
-   for(i=1;i<=n;i++){
-     cb();
-   }
+     if(n>0){
+       n=n-1;
+       cb();
+     }
   }
-  return invoke()
+  return {invoke};
 }
 function cb () { console.log("Callback invoked") };
-limitFunctionCallCount(3,cb);
+let n = limitFunctionCallCount(3,cb);
+n.invoke();
+n.invoke();
+n.invoke();
+n.invoke();
+n.invoke();
+n.invoke();
 
 
 
 //#3
 function cacheFunction(cb) {
-  function invoke(){
-  arr=[1,4,3,4,1,5,5];
-  cache={};
-  for(let x of arr){
+  let cache={};
+  function invoke(x){
     if(cache[x]){
       console.log(cache[x],"cache");
     }else{
@@ -45,10 +50,15 @@ function cacheFunction(cb) {
       console.log(y,"cb")
     }
   }
-  }
-  return invoke();
+  return {invoke}
 }
 
-cacheFunction(function(n){
+let s= cacheFunction(function(n){
   return n*2;
 })
+
+s.invoke(1);
+s.invoke(2);
+s.invoke(3);
+s.invoke(2);
+s.invoke(3);
